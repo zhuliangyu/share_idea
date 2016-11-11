86916a3346e528c26cf4b9362d1aa8c6d4a200a2
RickyZhu <riozhu@gmail.com>
2016-11-10 21:40:15 -0800

    jquery code

--- js_test.js
+++ js_test.js
@@ -1,19 +1,14 @@
-Visit this page http://vancouver.craigslist.ca and write a piece of jQuery code that changes all the links on the home page to have a color red if the link's anchor text includes the pattern eal (5%)
-
-```javascript
+// Visit this page http://vancouver.craigslist.ca and write a piece of jQuery code that changes all the links on the home page to have a color red if the link's anchor text includes the pattern eal (5%)
 
 $("a:contains('eal')").css("color", 'red');
-````
 
-Visit a Wikipedia page like this one: https://en.wikipedia.org/wiki/Rule_of_three_(computer_programming) then write a peice of jQuery code that will fadeOut any link you click on without following the link. (5%)
 
-```
+//     Visit a Wikipedia page like this one: https://en.wikipedia.org/wiki/Rule_of_three_(computer_programming) then write a peice of jQuery code that will fadeOut any link you click on without following the link. (5%)
+
  $('a').on('click', function (event) {
     event.preventDefault();
     $(this).fadeOut(100);
 });
-```
-
 
 
 //     Write a Javascript code to do the following: Implement filter function in Javascript (5%)
@@ -22,7 +17,6 @@ Visit a Wikipedia page like this one: https://en.wikipedia.org/wiki/Rule_of_thre
 // filter([1, 2, 3, 4], isEven);
 // => [2, 4]
 
-```
 var isEven = function (x) {
     return x % 2 == 0;
 };
@@ -41,4 +35,3 @@ function filter(arr, fn) {
 }
 
 console.log(filter([1, 2, 3, 4], isEven));
-```
